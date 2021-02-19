9758c9aaafd1f59f3d3ce31b6b5fa56d764c2611
RaphaelRPera <raphael.r.pera@gmail.com>
2021-02-19 20:27:20 -0300

    front - ajuste na conexão com a api

--- Jotapeg-front/src/services/api.js
+++ Jotapeg-front/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseUrl = 'http://ec2-54-227-230-170.compute-1.amazonaws.com:3003'
+export const baseUrl = 'http://ec2-54-227-230-170.compute-1.amazonaws.com'
 
 export default axios.create({
     baseURL: baseUrl
